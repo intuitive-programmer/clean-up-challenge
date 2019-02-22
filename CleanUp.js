ecf616d8394198f63a43670681724495e0a679ff
Steven Rico <steven@intuitiveprogrammer.com>
2019-02-22 14:25:10 +0000

    Assigned data to various variables. Created getCoords with a test.

--- CleanUp.js
+++ CleanUp.js
@@ -8,8 +8,31 @@ const extractData = path => {
 
 const cleanUpData = extractData('./input001.txt')
 
-console.log(cleanUpData)
+/* ASSIGN DATA TO VARIABLES */
 
-module.exports = {
-  extractData
+const getCoords = data => {
+  const coords = data.split(" ")
+  return {
+    x: parseInt(coords[0]),
+    y: parseInt(coords[1])
+  }
+}
+
+const rows = cleanUpData.split("\n")
+
+const roomDimensions = getCoords(rows[0])
+
+const hooverPosition = getCoords(rows[1])
+
+const patchesOfDirtRows = rows.slice(2, -1)
+const patchesOfDirt = patchesOfDirtRows.map(getCoords)
+
+const last = rows.length - 1
+const instructions = rows[last].split("")
+
+console.log(roomDimensions, hooverPosition, patchesOfDirt, instructions)
+
+ module.exports = {
+  extractData,
+  getCoords
 }
--- CleanUp.test.js
+++ CleanUp.test.js
@@ -1,5 +1,12 @@
-const { extractData } = require("./CleanUp")
+const {
+  extractData,
+  getCoords
+} = require("./CleanUp")
 
 test("extractData returns a string", () => {
   expect(typeof extractData("./input001.txt")).toBe("string")
+})
+
+test("getCoords returns an object with x and y coordinates", () => {
+  expect(getCoords("2 3")).toMatchObject({ x: 2, y: 3 })
 })
